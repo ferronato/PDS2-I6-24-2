5ebb5ed9c0778e44377635ea528a7a6c9bdbe54e
Jair Ferronato <ferronatto11@hotmail.com>
2024-09-25 19:59:40 -0300

    Add files via upload
    Ajustes na Classe

---
+++ Assistec/src/main/java/br/com/loja/assistec/App.java
@@ -0,0 +1,11 @@
+package br.com.loja.assistec;
+
+import br.com.loja.assistec.view.LoginView;
+
+
+public class App {
+    public static void main(String[] args) {
+    	LoginView view = new LoginView();
+		view.setVisible(true);
+    }
+}
