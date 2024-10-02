bb22da58494590c7583663bc6700360333f03c75
Jair Ferronato <ferronatto11@hotmail.com>
2024-10-02 11:07:35 -0300

    Update ConexaoBD.java

--- Assistec/dal/ConexaoBD.java
+++ Assistec/dal/ConexaoBD.java
@@ -3,22 +3,29 @@ package br.com.loja.assistec.dal;
 import java.sql.*;
 
 public class ConexaoBD {
+	// São criadas constantes pois as informações são estáticas
+	private static final String URL_MYSQL = "jdbc:mysql://localhost:3306/lojabd";
+
+	private static final String DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
+
+	private static final String USER = "root";
+
+	private static final String PASSWORD = "aluno";
+
 	// método para estabelecer conexão com o BD
 	public static Connection getConnection() {
-		Connection conexao = null;
-		// chamar o driver
-		String driver = "com.mysql.cj.jdbc.Driver";
-		// Armazenando infos do banco
-		String url = "jdbc:mysql://localhost:3306/lojabd";
-		String user = "root";
-		String password = "aluno";
+
 		// Estabelecer a conexao com o DB
 		try {
-			Class.forName(driver);
-			conexao = DriverManager.getConnection(url, user, password);
-			return conexao;
-		} catch (Exception e) {
-			return null;
+			Class.forName(DRIVER_CLASS);
+			return DriverManager.getConnection(URL_MYSQL, USER, PASSWORD);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
 		}
+		return null;
 	}
+	
+	
 }
